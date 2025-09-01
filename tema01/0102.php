f2be4dd14ae36740a4df267b82ee5fb7b541c4b5
Adel Eizeddin <adeleizeddin@MacBook-Air-alkhas-b-Adel.local>
2025-09-01 14:41:20 +0200

    oppgave 2

---
+++ tema01/0102.php
@@ -0,0 +1,10 @@
+<?php    /* Eksempel 1 */
+/*
+/*    Programmet mottar fra et HTML-skjema et fornavn og et etternavn ved POST-metoden
+/*    Programmet skriver ut en "god dag"-melding med personens navn 
+*/
+  $fornavn=$_POST ["fornavn"];
+  $etternavn=$_POST ["etternavn"];  
+  $alder=$_POST ["alder"];
+  print ("God dag $fornavn $etternavn. du er $alder år og like sprek<br />");  
+?>
